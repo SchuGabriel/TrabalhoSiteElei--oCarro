f77264b9e53e149305bdc947424b1f9c0b095b42
Gabriel Schu <schu-gabriel@hotmail.com>
2024-03-31 16:32:54 -0300

    Site completo

---
+++ script/script.js
@@ -0,0 +1,6 @@
+<script src="https://cdn.jsdelivr.net/npm/howler@2.3.5/dist/howler.min.js"></script>
+
+var sound = new Howl({
+    src: ['honda-fk8-civic-type-r-acceleration-w-valvetronic-catless-fi-exhaust-x-one-belief-automod'], // Substitua com o URL do seu arquivo MP3
+    volume: 0.5, // 1.0 é o volume máximo, você pode ajustar esse valor
+});
